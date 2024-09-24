9f0de3394b596e41607389ab7473080460cb23f7
vinnie <160987291+nnieseeghosts@users.noreply.github.com>
2024-09-24 13:34:36 -0300

    comentarios

--- Testes/main.c
+++ Testes/main.c
@@ -3,24 +3,24 @@
 #include <locale.h>
 #include <time.h>
 
-void gerar(int *lista);
+void gerar(int *lista); //Crio a função gerar
 
 int main(){
-    setlocale(LC_ALL, "PORTUGUESE");
+    setlocale(LC_ALL, "PORTUGUESE"); //Defino a linguagem ed revisão como português
 
-    int tela[9];
-    gerar(tela);
+    int tela[9]; //Crio a variável do array (pode ser global se quiserem)
+    gerar(tela); //Com a função, transformo o array criado em uma tela resolvível
 
-    for(int i = 0; i < 9; i++){
+    for(int i = 0; i < 9; i++){ //Printo o array a fim de testes
         printf("%d", tela[i]);
     }
 }
 
 
-void gerar(int *lista){
+void gerar(int *lista){ //Defino a variável gerar
 
-    int valor = 0;
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios
+    int valor = 0; //Crio a variável valor já no começo, pois ela será o valor lógico do While
+    srand(time(NULL)); // Inicializa o gerador de números aleatórios (Utilizando o fator random do relógio do computador)
     while (valor != 1){
 
         // GERAR LISTA:
@@ -40,28 +40,28 @@ void gerar(int *lista){
 
         //CALCULAR INVERSÕES:
 
-        int inv = 0;
+        int inv = 0; //Defino a variável que contará o número de inversões
         for(int i = 0; i < 8; i++)
         {
-            for(int j = i + 1; j < 9; j++ )
+            for(int j = i + 1; j < 9; j++ ) //Crio um for dentro de outro para verificar todas as combinações de dois valores do array
             {
-                if(lista[i] > lista[j])
+                if(lista[i] > lista[j] && lista[i] != 0 && lista[j] != 0) //Se houverem inversões de posição (Um valor maior que o outro na lista) e diferente de 0 (Zero é o vazio):
                 {
-                    inv++;
+                    inv++; //Adiciono +1 na contagem de reversões
                 }
             }
         }
 
         //VALIDAR
-        for(int i = 0; i < 9; i++){
-            if(lista[i] == 0){
-                if((i+1) % 2 == 0 && inv % 2 == 0){
-                    valor = 1;
+        for(int i = 0; i < 9; i++){ //Crio um for para ler a lista
+            if(lista[i] == 0){ //Acho o vazio
+                if((i+1) % 2 == 0 && inv % 2 == 0){ //Verifico se a posição inicial do vazio é par e se o número de inversões também é
+                    valor = 1; //Caso seja, a tela gerada é válida
                 }
-                else{
-                    if((i+1) % 2 == 1 && inv % 2 == 1){
-                        valor = 1;
-                    }
+                else{ //Caso contrário:
+                    if((i+1) % 2 == 1 && inv % 2 == 1){ //Verifico se a posição atual do vazio é impar e se o número de inversões também
+                        valor = 1; //Caso seja, a tela gerada é válida
+                    } //Caso o valor gerado não cumpra nenhum dos dois requisitos, a tela gerada não é resolvível, então a função continua gerando até ser
                 }
             }
 
