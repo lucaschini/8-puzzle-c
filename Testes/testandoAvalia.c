97c3daa78706e169bd59d34139d6f04a4c7b62cb
Lucas Chini <lucaschinimendonca@protonmail.com>
2024-09-28 11:31:00 -0300

    Avaliação com ponteiro pronta!

--- Testes/testandoAvalia.c
+++ Testes/testandoAvalia.c
@@ -2,39 +2,35 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void avalia();
+void avalia(int m_comparar[3][3], int *retorno);
 
 int main()
 {
     setlocale(LC_ALL,"portuguese");
 
-    int teste[3][3] = {{1,2,3},{4,5,6},{7,8,0}}, resultado = 2, v_procurado[3][3] = {{1,2,3},{4,5,6},{7,8,0}};
+    int teste[3][3] = {{1,2,3},{4,5,6},{7,8,0}}, resultado = 2, *retorno;
 
 
-    avalia();
-//    printf("Resultado da avaliação %d",resultado);
+    avalia(teste, &retorno);
+    printf("Resultado da avaliação %d",retorno);
 
     return 0;
 }
 
-void avalia()
+void avalia(int m_comparar[3][3], int *retorno)
 {
-    int v_procurado[3][3] = {{1,2,3},{4,5,6},{7,8,0}}, sum = 0, teste[3][3] = {{1,2,3},{4,5,6},{7,0,8}}; // usar soma pra verificar quantos numeros estão em uma posição correta
+    int v_procurado[3][3] = {{1,2,3},{4,5,6},{7,8,0}}, sum = 0; // usar soma pra verificar quantos numeros estão em uma posição correta
 
     for(int i=0; i<3; i++){
-            printf("\n");
         for(int j=0; j<3; j++){
-            if(v_procurado[i][j] == teste[i][j]){
-                printf("%d \n", v_procurado[i][j]);
-                printf("%d \n", teste[i][j]);
+            if(v_procurado[i][j] == m_comparar[i][j]){
                 sum++;
             }
         }
     }
-    printf("Resultado da soma: %d",sum);
-//    if (sum > 0){
-//        return 1; // caso a soma seja 9, todos os valores estão na posição correta, a solução foi encontrada
-//    } else {
-//          return 0; // caso o vetor atual nao seja igual ao objetivo, ele retorna 0, ou seja, a solução não foi encontrada
-//    }
+    if (sum == 9){
+            *retorno = 1; // caso a soma seja 9, todos os valores estão na posição correta, a solução foi encontrada
+    } else {
+        *retorno = 0; // caso o vetor atual nao seja igual ao objetivo, ele retorna 0, ou seja, a solução não foi encontrada
+    }
 }
