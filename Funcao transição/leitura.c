7b6a29e9b670a909601f16eabf022d66d3fa7828
ViniciusPinheiroLivinalli <viniciuspinheirolivinalli@gmail.com>
2024-09-30 16:30:12 -0300

    Função sucessora completa
    joia.

--- Funcao transição/leitura.c
+++ Funcao transição/leitura.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <math.h>
-#include <conio.h>
 
 #define KEY_UP 72
 #define KEY_DOWN 80
@@ -14,30 +13,43 @@ void sucessora(int *movimento, int *i, int *j, int matriz[3][3]){
     int aux_valor, aux_i, aux_j;
     aux_i = *i; //guardando a posição inicial do vazio
     aux_j = *j;
-    if(*movimento == KEY_UP && (*i) > 0){ //fazendo a verificação e alterando a posição
+
+    if (*movimento == KEY_UP && (*i) > 0) { //fazendo a verificação e alterando a posição
         (*i)--;
     }
-    else if(*movimento == KEY_DOWN && (*i) < 2){
+    else if (*movimento == KEY_DOWN && (*i) < 2) {
         (*i)++;
     }
-    else if(*movimento == KEY_RIGHT && (*j) < 2){
+    else if (*movimento == KEY_RIGHT && (*j) < 2) {
         (*j)++;
     }
-    else if(*movimento == KEY_LEFT && (*j) > 0){
+    else if (*movimento == KEY_LEFT && (*j) > 0) {
         (*j)--;
     }
-    else{
+    else {
         return; // se não for válido
     }
+
     aux_valor = matriz[*i][*j]; //guardando o valor que o vazio vai ocupar futuramente
     matriz[*i][*j] = 0; //definindo a nova posição como vazio
     matriz[aux_i][aux_j] = aux_valor; //definindo a posição antiga do vazio com o novo valor
-    return;
 }
 
-int tecla = KEY_LEFT, i = 2, j = 1;
-int m[3][3] = {{1,2,3},{4,5,6},{7,8,0}};
-int main(){
-sucessora(&tecla,&i,&j,m);
-printf("%d%d",i,j);
+int main() {
+    int tecla = KEY_LEFT, i = 2, j = 2;
+    int m[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+
+    sucessora(&tecla, &i, &j, m);
+
+    printf("Nova posição do vazio: (%d, %d)\n", i, j);
+
+    printf("Matriz atualizada:\n");
+    for (int linha = 0; linha < 3; linha++) {
+        for (int coluna = 0; coluna < 3; coluna++) {
+            printf("%d ", m[linha][coluna]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
